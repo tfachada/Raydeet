b407bca08449e07d4c2ed99290563b1837403c00
Tomás Fachada <tom.fachada@gmail.com>
2022-11-01 00:07:43 +0000

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Homepage for Raydeet
+
+Web development project. Any similarity to old.reddit.com is pure coincidence.
+
+https://github.com/HackerSchool/recrutamento-22-23-s1/tree/main/webdev
---
+++ loadposts.js
@@ -0,0 +1,30 @@
+const post = document.getElementById("article-template").content
+
+titles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
+sources = ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+ages = ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+ops = ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+subs = ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+comment_numbers = ["1", "2", "3", "4", "5", "0", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+
+for (var i = 0; i < 25; i++) {
+  var copy = document.importNode(post, true);
+
+  copy.querySelector(".title").textContent = titles[i];
+  copy.querySelector(".post-source").textContent = "("+sources[i]+")";
+  copy.querySelector(".age").textContent = ages[i];
+  copy.querySelector(".OP").textContent = ops[i];
+  copy.querySelector(".sub").textContent = subs[i];
+  if (comment_numbers[i] == 0) {
+    copy.querySelector(".ncomments").textContent = "";
+    copy.querySelector(".plural").textContent = "";
+  }
+  else {
+    copy.querySelector(".ncomments").textContent = comment_numbers[i] + " ";
+    if (comment_numbers[i] == 1) {
+      copy.querySelector(".plural").textContent = "";
+    }
+  }
+  
+  document.getElementById("post-list").appendChild(copy);
+}
